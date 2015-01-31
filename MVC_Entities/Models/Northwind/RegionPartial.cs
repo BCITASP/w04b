c19cf2884573b6ec97a794fab99d8338bf5d19d7
alan-cy-lai <alan@ergostratus.com>
2015-01-30 20:57:13 -0800

    Added Annotations & Changed Theme - Partial class used to modify region settings     - Required     - Display name changed to Rgn Desc     - Check for 10 characters max - Changed bootstrap theme to "superhero" from bootswatch.com

---
+++ MVC_Entities/Models/Northwind/RegionPartial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Entities.Models.Northwind
+{
+    [MetadataType(typeof(RegionAttributes))]
+    public partial class Region
+    {
+
+    }
+
+    public class RegionAttributes
+    {
+
+        public int RegionID { get; set; }
+        [Required]
+        [Display(Name = "Rgn Desc")]
+        [MaxLength(10, ErrorMessage = "Length must not exceed {1}")]
+        public string RegionDescription { get; set; }
+    }
+}
---
+++ MVC_Entities/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Entities.Startup))]
+namespace MVC_Entities
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
